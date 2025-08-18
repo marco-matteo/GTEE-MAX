a22f01e8d7471eca48fbc6d01f6c5d4522c20821
Marco Matteo <marco.matteo@edu.tbz.ch>
2025-08-18 09:51:06 +0200

    init

---
+++ backend/src/main/kotlin/com/max/gtee/gteemax/GteemaxApplication.kt
@@ -0,0 +1,11 @@
+package com.max.gtee.gteemax
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GteemaxApplication
+
+fun main(args: Array<String>) {
+	runApplication<GteemaxApplication>(*args)
+}
