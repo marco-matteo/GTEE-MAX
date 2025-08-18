55a5224607e40e4f2779a815f3b3148c49281c5a
skossel <143590242+skossel@users.noreply.github.com>
2025-08-18 16:05:00 +0200

    add github pipeline + vitest in frontend
    Co-authored-by: Marco Matteo <marco.matteo@edu.tbz.ch>

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest"
+
+test("this just for a successfull pipeline", () => {
+    expect("sum").toBe("sum");
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-function App() {
-  return (
-    <h1 className='bg-black text-white'>Hello World</h1>
-  )
-}
+export default function App() {
+    return <h1 className="bg-black text-white">Hello World</h1>;
 
-export default App
+}
