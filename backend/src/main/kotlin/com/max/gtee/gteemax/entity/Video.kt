5011e795ba4157fc15575a33c2eac010329d7bcf
mtraub <mario.traub@ergon.ch>
2025-08-25 09:46:28 +0200

    #13: create jpa entities & use application.yml instead of application.properties

---
+++ backend/src/main/kotlin/com/max/gtee/gteemax/entity/Video.kt
@@ -0,0 +1,19 @@
+package com.max.gtee.gteemax.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Video(
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    var id: Int? = null,
+
+    val path: String?,
+
+    val views: Int,
+
+    val caption: String?,
+)
