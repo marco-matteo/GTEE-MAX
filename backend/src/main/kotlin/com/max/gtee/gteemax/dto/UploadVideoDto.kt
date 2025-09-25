b6deab7402ff42bdb0be1d89d01e04ec311d77de
Marco Matteo <marco.matteo@edu.tbz.ch>
2025-09-25 09:20:53 +0200

    add upload video frontend

--- backend/src/main/kotlin/com/max/gtee/gteemax/dto/UploadVideoDto.kt
+++ backend/src/main/kotlin/com/max/gtee/gteemax/dto/UploadVideoDto.kt
@@ -4,6 +4,6 @@ import org.springframework.web.multipart.MultipartFile
 
 data class UploadVideoDto(
     val creatorId: String,
-    val caption: String?,
+    val caption: String,
     val videoFile: MultipartFile,
 )
