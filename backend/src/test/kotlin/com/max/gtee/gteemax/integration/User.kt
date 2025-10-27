312a039b62ef3705913037786151c9d4db7be461
Sandro Kossel <sandro.kossel@ergon.ch>
2025-10-27 10:08:33 +0100

    #42: ktlint format

--- backend/src/test/kotlin/com/max/gtee/gteemax/integration/User.kt
+++ backend/src/test/kotlin/com/max/gtee/gteemax/integration/User.kt
@@ -1,19 +1,18 @@
 package com.max.gtee.gteemax.integration
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class User {
-
     @Autowired
     lateinit var mockMvc: MockMvc
 
@@ -24,7 +23,7 @@ class User {
         mockMvc.perform(
             post("/user/register")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(json)
+                .content(json),
         )
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.username").value("sandro"))
