4b89df3221d31e756e143155e776416e669bb709
mariotraub <150661383+mariotraub@users.noreply.github.com>
2025-11-03 10:18:35 +0100

    Feat/#46 it (#52)
    * #46: first it
    
    * #46 add another user test
    
    ---------
    
    Co-authored-by: mtraub <mario.traub@ergon.ch>

--- backend/it/test_user.py
+++ backend/it/test_user.py
@@ -12,3 +12,22 @@ def test_register():
     data = res.json()
     assert data["username"] == "sandro"
     assert data["password"] != "password"
+
+
+def test_login():
+    user = {"username":"mario","password":"password"}
+    register_url = f"{BASE_URL}/user/register"
+    _ = requests.post(
+        register_url, 
+        json=user
+    )
+
+    url = f"{BASE_URL}/user/login"
+    
+    res = requests.post(
+        url,
+        json=user
+    )
+
+    assert res.status_code == 200
+    assert res.headers["set-cookie"].startswith("jwt=")
